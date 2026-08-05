ff2f59b9d80c7887d7111f21516c0e940b96afe6
SimpalChowdhary <23251a1229@gmail.com>
2026-08-05 09:50:09 +0530

    Correct print statement and add Jenkins message
    Fix typo in print statement and add new output.

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello{
     public static void main(String[] args){
         System.out.prinltn("Hello World");
         System.out.prinltn("Welcome to java program");
-
+        System.out.println("Running java program in jenkins");
     }
 }
